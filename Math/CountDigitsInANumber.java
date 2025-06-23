affe7757606fef46fb17ce99835b106bddf4289e
Asheesh <amahapatro0@gmail.com>
2025-06-23 12:16:07 +0530

    counting the digits in a number

--- Math/CountDigitsInANumber.java
+++ Math/CountDigitsInANumber.java
@@ -1,5 +1,20 @@
+import java.util.*;
 class CountDigitsInANumber{
+
+    public static int countDigits(int n){
+        int count = 0;;
+        while(n!=0){
+            n = n/10;
+            count++;
+
+        }  
+        return count;
+    }
     public static void main(String[]args){
-        System.out.println("Hello");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n = sc.nextInt();
+        System.out.println("input is:"+ n);
+        System.out.println(countDigits(n));
     }
 }
